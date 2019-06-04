4f7e63963343f4cbdd9fb4e6d2969df6b78bb3ba
Rosler <oriolr32@gmail.com>
2019-06-03 23:29:20 -0400

    Server.js Userlist -> Userslist fix

--- server/server.js
+++ server/server.js
@@ -25,6 +25,45 @@ let schema = buildSchema(`
     }
 `);
 
+
+// Maps id to User object
+let userslist = {
+    a: {
+        id: "a",
+        nickname: "Rosler",
+        avatar: "https://avatars0.githubusercontent.com/u/11508340?s=460&v=4"
+    },
+};
+
+let postslist = {
+    a: {
+        a:{
+            id: "a",
+            user: userslist["a"],
+            caption: "Growing one project at a time",
+            image: "https://static01.nyt.com/images/2018/06/26/arts/design/26banksy2/merlin_140236308_c92f6d2c-a041-46b7-b660-09fabce9ed5a-superJumbo.jpg?quality=90&auto=webp"
+        },
+        b: {
+            id: "b",
+            user: userslist["a"],
+            caption: "Angular Book :)",
+            image: "https://cdn-images-1.medium.com/max/1000/1*ltLfTw87lE-Dqt-BKNdj1A.jpeg"
+        },
+        c: {
+            id: "c",
+            user: userslist["a"],
+            caption: "Me at Frontstack.io",
+            image: "https://pbs.twimg.com/media/DNNhrp6W0AAbk7Y.jpg:large"
+        },
+        d: {
+            id: "d",
+            user: userslist["a"],
+            caption: "Moviing the community",
+            image: "https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg"
+        }
+    }
+};
+
 //The root provides a resolver function for each API endpoint
 let root = {
     user: function({ id }) {
@@ -50,41 +89,3 @@ app.use(
 );
 // set application port
 app.listen(4000);
-
-// Maps id to User object
-let userslist = {
-    a: {
-        id: "a",
-        nickname: "Rosler",
-        avatar: "https://avatars0.githubusercontent.com/u/11508340?s=460&v=4"
-    },
-};
-
-let postslist = {
-    a: {
-        a:{
-            id: "a",
-            user: userlist["a"],
-            caption: "Growing one project at a time"
-            image: "https://static01.nyt.com/images/2018/06/26/arts/design/26banksy2/merlin_140236308_c92f6d2c-a041-46b7-b660-09fabce9ed5a-superJumbo.jpg?quality=90&auto=webp"
-        },
-        b: {
-            id: "b",
-            user: userlist["a"],
-            caption: "Angular Book :)",
-            image: "https://cdn-images-1.medium.com/max/1000/1*ltLfTw87lE-Dqt-BKNdj1A.jpeg"
-        },
-        c: {
-            id: "c",
-            user: userlist["a"],
-            caption: "Me at Frontstack.io",
-            image: "https://pbs.twimg.com/media/DNNhrp6W0AAbk7Y.jpg:large"
-        },
-        d: {
-            id: "d",
-            user: userlist["a"],
-            caption: "Moviing the community",
-            image: "https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg"
-        }
-    }
-};
